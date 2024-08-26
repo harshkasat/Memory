b84eae2bf226aebd8135895a8cbcb855271ad0a9
Harsh Kasat <harshkasat01@gmail.com>
2024-08-26 12:34:24 +0530

    initial commit

--- user/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import CustomUser, Album, Media, Notification, TempMedia
-# Register your models here.
-
-admin.site.register(CustomUser)
-admin.site.register(Album)
-admin.site.register(Media)
-admin.site.register(Notification)
-admin.site.register(TempMedia)
--- user/router.py
+++
@@ -1,7 +0,0 @@
-from rest_framework.routers import DefaultRouter
-from .views import CustomViewSet, AlbumViewSet
-
-router = DefaultRouter()
-
-router.register(r'createuser', CustomViewSet)
-router.register(r'albums', AlbumViewSet, basename='album')
